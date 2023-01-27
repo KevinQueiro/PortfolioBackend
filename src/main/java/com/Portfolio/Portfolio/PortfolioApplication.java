31e1b489f9d4074f1790b7a43622ffde043a8b24
KevinQueiro <kevin.queiro@outlook.com>
2023-01-26 22:05:18 -0300

    try 4

--- src/main/java/com/Portfolio/Portfolio/PortfolioApplication.java
+++ src/main/java/com/Portfolio/Portfolio/PortfolioApplication.java
@@ -2,6 +2,9 @@ package com.Portfolio.Portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -10,4 +13,16 @@ public class PortfolioApplication {
         SpringApplication.run(PortfolioApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+                                        .allowedOrigins("https://kq-portfolio.firebaseapp.com")
+                                        .allowedMethods("*")
+                                        .allowedHeaders("*");
+			}
+		};
+	}
 }
